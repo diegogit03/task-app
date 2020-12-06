216db1276072e706e6f1e0e91946736ff49d0e35
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-06 16:58:02 -0300

    task list component

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import TaskList from './src/screens/TaskList';
+
+export default class App extends Component {
+  render() {
+    return (
+      <TaskList />
+    );
+  }
+}
